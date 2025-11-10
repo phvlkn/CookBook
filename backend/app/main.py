a98f8a886816bacfcd5eb14bc07ddedd56da76ee
phvlkn <eminislamow@googlemail.com>
2025-11-10 16:25:41 +0300

    докер 2

--- backend/app/main.py
+++ backend/app/main.py
@@ -131,4 +131,4 @@ if __name__ == "__main__":
         create_seed_data()
     except Exception as e:
         print(f"Seeding failed: {e}")
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
